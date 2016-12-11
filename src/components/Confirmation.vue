50c94dbfc4034f38093c3a00937308b9cd610ddc
mimani <mimani.saurabh@gmail.com>
2016-12-11 19:46:20 +0530

    Reverted vel's changes, moved those changes to separate branch

--- src/components/Confirmation.vue
+++ src/components/Confirmation.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="container">
-
-                     <h1>Carousel <small>Bootstrap Visual Test</small></h1>
-                     <p>Also, the carousel shouldn't slide when its window/tab is hidden. Check the console log.</p>
-                     <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                       <ol class="carousel-indicators">
-                         <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                         <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
-                         <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
-                       </ol>
-                       <div class="carousel-inner">
-                         <div class="carousel-item active">
-                           <img alt="First slide" src="https://37.media.tumblr.com/tumblr_m8tay0JcfG1qa42jro1_1280.jpg">
-                         </div>
-                         <div class="carousel-item">
-                           <img alt="Second slide" src="https://37.media.tumblr.com/tumblr_m8tazfiVYJ1qa42jro1_1280.jpg">
-                         </div>
-                         <div class="carousel-item">
-                           <img alt="Third slide" src="https://38.media.tumblr.com/tumblr_m8tb2rVsD31qa42jro1_1280.jpg">
-                         </div>
-                       </div>
-                       <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                         <span class="glyphicon glyphicon-chevron-left"></span>
-                       </a>
-                       <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                         <span class="glyphicon glyphicon-chevron-right"></span>
-                       </a>
-                     </div>
-
-                   </div>
+	<div row>
+    <div class="card col-xs-5">
+      <div id="carousel-example-generic" class="carousel slide product-img" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+          <div class="carousel-item active">
+            <!-- <img src="../../assets/devices/TV.png" alt="First slide"> -->
+            <img src="http://2.bp.blogspot.com/-pATX0YgNSFs/VP-82AQKcuI/AAAAAAAALSU/Vet9e7Qsjjw/s1600/Cat-hd-wallpapers.jpg" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <!-- <img src="../../assets/devices/WashingMachine.png" alt="Second slide"> -->
+            <img src="http://newsyac.com/wp-content/uploads/2016/01/worried-cat.jpg" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img src="http://2.bp.blogspot.com/-pATX0YgNSFs/VP-82AQKcuI/AAAAAAAALSU/Vet9e7Qsjjw/s1600/Cat-hd-wallpapers.jpg" alt="Third slide">
+          </div>
+        </div>
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="icon-prev" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="icon-next" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+	</div>
 </template>
 
 <script type="text/javascript">
